d06f3941ea32fd41a169a2aa8c4b86e6d04193e2
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2024-09-18 13:50:37 +0500

    added journal entry card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,61 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Text,
+} from "@chakra-ui/react";
 import { PlusIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        height: "40vh",
-        mx: "30",
-      }}
-    >
-      <Text fontSize="2xl">
-        Recent Entries
-      </Text>
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          height: "40vh",
+          mx: "30",
+        }}
+      >
+        <Text fontSize="2xl">Recent Entries</Text>
 
-      <Button color="white" bg="black" _hover={{
-        bg: "gray.800",
-        shadow: "md",
-      }}>
-        <PlusIcon />
-        New Entry
-      </Button>
+        <Button
+          color="white"
+          bg="black"
+          _hover={{
+            bg: "gray.800",
+            shadow: "md",
+          }}
+        >
+          <PlusIcon />
+          New Entry
+        </Button>
+      </Box>
+
+      <Card
+        sx={{
+          mx: "30",
+          my: "10",
+          p: "4",
+          boxShadow: "md",
+        }}
+      >
+        <CardHeader fontWeight="bold" fontSize="2xl">
+          Entry 1
+        </CardHeader>
+        <CardBody>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum
+            officiis reprehenderit quae numquam. Vero vitae quia nesciunt sit
+            suscipit temporibus similique dolores? Asperiores, error.
+            Praesentium ex minus id consequuntur.
+          </Text>
+        </CardBody>
+      </Card>
     </Box>
   );
 }
